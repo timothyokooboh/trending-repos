152094178d008b0f9c978d9e0331ddaea4a349ab
timothy okooboh <okoobohtimothy@gmail.com>
2021-04-02 01:49:20 +0100

    Design Header component
    Design a header component that is fixed at the top of the page
    Please enter the commit message for your changes. Lines starting

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+    <the-header></the-header>
     <trending-repos-list></trending-repos-list>
   </div>
 </template>
 
 <script>
 import TrendingReposList from "./components/TrendingReposList";
+import TheHeader from "./components/TheHeader"
 
 export default {
   name: 'App',
   components: {
-    TrendingReposList
+    TrendingReposList,
+    TheHeader
   },
 }
 </script>
